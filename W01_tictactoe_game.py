448b45937a8dabc7f54f7ffd0bc6b0590e395a11
Hilmar Orozco <oro18005@byui.edu>
2022-06-14 11:48:43 -0600

    Updated version of tictactoe game.

--- W01_tictactoe_game.py
+++ W01_tictactoe_game.py
@@ -4,56 +4,46 @@
 2. Class Name:
     CSE 210-13 Programming with Classes.
 3. Assignment Name: 
-    CSE_210_tictactoe_solution.py
+    CSE_210_tictactoe_game.py
 4. Assignment Description:
     This is a tic tac toe game program.
 """
 
-def create_board():
-    board = []
-    for square in range(9):
-        board.append(square + 1)
-    return board
+# 1. Name: 
+#       Hilmar Orozco
+# 2. Assignment Name: 
+#       CSE_210_tictactoe_solution.py
+# 3. Assignment Description:
+#       This is a tic tac toe game program.
 
 def display_board(board):
-    print("\n       |       |    ")
-    print(f"   {board[0]}   |   {board[1]}   |   {board[2]}")
-    print("       |       |    \n-------+-------+-------")
-    print("       |       |    ")
+    print(f"\n       |       |\n   {board[0]}   |   {board[1]}   |   {board[2]}")
+    print(f"       |       |\n-------+-------+-------\n       |       |") 
     print(f"   {board[3]}   |   {board[4]}   |   {board[5]}")
-    print("       |       |    \n-------+-------+-------")
-    print("       |       |    ")
-    print(f"   {board[6]}   |   {board[7]}   |   {board[8]}")
-    print("       |       |    \n")
+    print(f"       |       |\n-------+-------+-------\n       |       |")
+    print(f"   {board[6]}   |   {board[7]}   |   {board[8]}\n       |       |") 
     
-def has_winner(board):
-    return (board[0] == board[1] == board[2] or
-            board[3] == board[4] == board[5] or
-            board[6] == board[7] == board[8] or
-            board[0] == board[3] == board[6] or
-            board[1] == board[4] == board[7] or
-            board[2] == board[5] == board[8] or
-            board[0] == board[4] == board[8] or
-            board[2] == board[4] == board[6])
-
-def make_move(player, board):
-    square = int(input(f"{player}'s turn to choose a square (1-9): "))
-    board[square - 1] = player
-
-def next_player(current):
-    if current == "" or current == "o":
-        return "x"
-    elif current == "x":
+def next_player(player):
+    if player == "x":
         return "o"
+    else:
+        return "x"
 
 def main():
-    player = next_player("")
-    board = create_board()
-    while not (has_winner(board)):
+    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    player = "x"
+    
+    while not (board[0] == board[1] == board[2] or board[3] == board[4] == board[5] or
+        board[6] == board[7] == board[8] or board[0] == board[3] == board[6] or
+        board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or
+        board[0] == board[4] == board[8] or board[2] == board[4] == board[6]):
+    
         display_board(board)
-        make_move(player, board)
+        position = int(input(f"It is {player}'s turn to choose position: "))
+        board[position - 1] = player
         player = next_player(player)
+        
     display_board(board)
-    print("Good game. Thanks for playing!") 
+    print("We have a winner!") 
 
 main()
